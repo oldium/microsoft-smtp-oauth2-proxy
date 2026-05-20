38e37848e4a4bd9551012271d7fa87a3f87e22f6
Oldřich Jedlička <oldium.pro@gmail.com>
2026-05-21 00:01:27 +0200

    Update dependencies
    Pin ESLint to v9 until eslint-plugin-react / eslint-plugin-react-hooks
    declare eslint v10 peer support.

--- .ncurc.js
+++ .ncurc.js
@@ -6,6 +6,13 @@ module.exports = {
       console.log(`\n👀  ️${dependencyName} is pinned to ${res}`)
       return  res;
     }
+    // Keep ESLint on v9 until eslint-plugin-react / eslint-plugin-react-hooks
+    // (pulled in by eslint-config-next) declare eslint v10 peer support.
+    if(["eslint", "@eslint/js"].includes(dependencyName)){
+      const res = "minor"
+      console.log(`\n👀  ️${dependencyName} is pinned to ${res} (eslint v9)`)
+      return  res;
+    }
     return 'latest'
   },
 }
